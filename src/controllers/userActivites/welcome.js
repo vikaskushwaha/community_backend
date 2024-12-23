57c671e0407374181d549d7b5b16fce969261054
vikaskushwaha <vikas.vikas.kushwaha@gmail.com>
2024-12-23 15:21:52 +0530

    %nullHandiling

--- src/controllers/userActivites/welcome.js
+++ src/controllers/userActivites/welcome.js
@@ -7,7 +7,26 @@ async function welcomePage(req, res) {
 
     const userData = await db.one('SELECT *FROM users WHERE id = $1', [userId])
     const userPoints = await db.one('SELECT *FROM userpoints WHERE id = $1', [userId])
-    const totalpercent = (userPoints.watched_video * 100) / 112;
+
+    let totalpercent;
+    if (userPoints.watched_video == null) {
+        totalpercent = 0;
+
+    }
+    else {
+        totalpercent = Math.floor((userPoints.watched_video.length * 100) / 112);
+    }
+    // let totalpercent;
+    // if (userPoints.watched_video != null) {
+    //     if ((userPoints.watched_video).length > 0) {
+    //         totalpercent = Math.floor((userPoints.watched_video.length * 100) / 112);
+    //     }
+    // }
+    // else {
+    //     totalpercent = 0;
+    // }
+
+
     const usersInfo = {
         totalpercent,
         ListOfWatchedVideos: userPoints.watched_video,
